9c202265dc90bff1c34edec4fc2e8b7e782a1a99
Josh Ferrara <scjosh2@gmail.com>
2015-09-14 09:15:04 -0700

    Updated example to provide Az/El

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 var SGP4 = require('./');
 
-// Sample ISS TLE Data
-var issLine1 = "1 25544U 98067A   15073.52509288  .00016392  00000-0  24500-3 0  9990";
-var issLine2 = "2 25544  51.6454 196.8789 0008842 100.5316 358.5831 15.55127532933300";
+// Sample ISS TLE Data from 14 September 2015
+var issLine1 = "1 25544U 98067A   15256.76500793  .00042673  00000-0  62367-3 0  9995";
+var issLine2 = "2 25544  51.6468   1.7516 0001021 349.4970 110.1741 15.55194220961827";
 
 // Create a satellite record
 var issSatRec = SGP4.twoline2rv(issLine1, issLine2, SGP4.wgs84());
@@ -16,16 +16,14 @@ function printPosition() {
     var positionAndVelocity = SGP4.propogate(issSatRec, now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
     
     // Prints ECI coordinates
-    //console.log(positionAndVelocity);
+    console.log(positionAndVelocity);
     
     // GMST required to get Lat/Long
     var gmst = SGP4.gstimeFromDate(now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-    //console.log(gmst);
-
+    
     // Geodetic coordinates
     var geodeticCoordinates = SGP4.eciToGeodetic(positionAndVelocity.position, gmst);
-    //console.log(geodeticCoordinates);
-
+    
     // Coordinates in degrees
     var longitude = SGP4.degreesLong(geodeticCoordinates.longitude);
     var latitude = SGP4.degreesLat(geodeticCoordinates.latitude);
@@ -40,6 +38,18 @@ function printPosition() {
     // 2pi * sqrt(Relative Height / Gravity of Earth * Mass of Earth)
     console.log('Oribital Period: ' + ((2 * Math.PI) * (geodeticCoordinates.height + 6378.135)) * (Math.sqrt((geodeticCoordinates.height + 6378.135)/398600.8)) / 60);
     
+    var observerPos = {
+        longitude: -117.61199249999999 * SGP4.deg2rad,
+        latitude: 33.4269728 * SGP4.deg2rad,
+        height: 1
+    };
+
+    var satEcf = SGP4.eciToEcf(positionAndVelocity.position, gmst);
+    var lookAngles = SGP4.topocentricToLookAngles(SGP4.topocentric(observerPos, satEcf));
+
+    console.log("Azimuth: " + lookAngles.azimuth * SGP4.rad2deg);
+    console.log("Elevation: " + lookAngles.elevation * SGP4.rad2deg);
+
     // Call printPosition in 750 ms
     setTimeout(printPosition, 750);
 }
